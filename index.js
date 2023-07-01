c6b5e8e9d3f565337e3d3c0a9568b6d7d89a75d7
unknown <sikderfahad>
2023-07-01 14:24:45 +0600

    create db and collection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
@@ -6,9 +7,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+console.log(process.env.DB_USER);
 
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.3zuhxgd.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zuhxgd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,7 +23,11 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
+
+    const database = client.db("summerCamp");
+    const allClassesCollection = database.collection("all-classes");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -30,7 +35,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
