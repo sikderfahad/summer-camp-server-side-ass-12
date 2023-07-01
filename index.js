cbe1e2d8e9cbffb4d2c4c120814b1c4af0d6ea56
unknown <sikderfahad>
2023-07-01 09:30:27 +0600

    check origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Express is Running...!");
+  res.send("Summer Camp is Running...!⚡");
 });
 
 app.listen(port, () => {
